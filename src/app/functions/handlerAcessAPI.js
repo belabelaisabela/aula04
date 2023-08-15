b153783c668107f9252e97389d71ba20e90a02ca
belabelaisabela <isabelagualberto7@gmail.com>
2023-08-15 17:26:56 -0400

    commit 1

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,38 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const lista = [
+    {
+        name: "Isabela",
+        email: "isabela@gmail.com",
+        password: "123",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        name: "Mendes",
+        email: "mendes@gmail.com",
+        password: "1234",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        name: "Gabriel",
+        email: "gabriel@gmail.com",
+        password: "12345",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    }
+]
 
+const getUserAuthenticated = (userLogin) => {
+    let usermach = [];
+
+    lista.map((user) => {
+        if(user.email === userLogin.email && user.password === userLogin.password){
+            usermach = user
+        }
+    })
+    return usermach
 }
 
 const getUsers = () =>{
-        
+        return lista
 }
 export { getUsers, getUserAuthenticated };
