aa9f74863a4cc40bd047ae9d945920e7683437b0
belabelaisabela <100858278+belabelaisabela@users.noreply.github.com>
2023-08-15 23:29:37 -0400

    Update

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,12 +1,18 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 
 export default async function Dashboard() {
-   
+   const listaa = getUsers()
     return (
+    <div>
         <div>
             <h1>Dashboard</h1>
 
         </div>
-            
+        <div>
+            {listaa.map((user) =>
+                <h3>{user.name}</h3>
+            )}
+        </div>
+    </div>
     );
 };
