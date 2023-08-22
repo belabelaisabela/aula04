d18b3d55076f4e0f53dd4fd51263d004e0b07b28
belabelaisabela <isabelagualberto7@gmail.com>
2023-08-22 17:39:13 -0400

    .

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,18 +1,14 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react";
+import { ListUsers } from "@/app/functions/componentes/ListUsers"
 
 export default async function Dashboard() {
-   const listaa = getUsers()
+   const listaa = await getUsers()
     return (
     <div>
-        <div>
-            <h1>Dashboard</h1>
-
-        </div>
-        <div>
-            {listaa.map((user) =>
-                <h3>{user.name}</h3>
-            )}
-        </div>
+            <Suspense fullback={<p>Carregando...</p>}>
+                <ListUsers users={users}/>
+            </Suspense>
     </div>
     );
 };
