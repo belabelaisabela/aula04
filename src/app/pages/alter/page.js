522a0a9273eccb6740e1a19c45572778e679010b
belabelaisabela <isabelagualberto7@gmail.com>
2023-09-12 23:12:45 -0400

    rotas funcionando

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -1,24 +1,20 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
-import  ListUsers  from "@/app/functions/componentes/ListUsers"
 import 'react-toastify/dist/ReactToastify.min.css';
 import Link from "next/link";
-import styles from './style.css'
 
-export default async function Dashboard() {
-   const listaa = await getUsers()
+
+export default async function Alter() {
     return (
     <div>
     <nav id="menu-h">
         <ul>
-            <li><Link href={"/pages/dashboard"}>Homee</Link></li>
+            <li><Link href={"/pages/dashboard"}>Home</Link></li>
             <li><Link href={"/pages/register"}>Register</Link></li>
             <li><Link href={"/pages/alter"}>Alterar</Link></li>
         </ul>
     </nav>
-            <Suspense fallback={<p>Carregando...</p>}>
-                <ListUsers users={listaa}/>
-            </Suspense>  
+       Alterar
     </div>
     );
 };
